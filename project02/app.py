ad36f832979ddd9a381c2c20d0848e4e3bcd4aeb
kimdaehyun <zbeld123@gmail.com>
2021-09-14 17:51:44 +0900

    단어 추가(save), 단어 삭제(delete)

--- project02/app.py
+++ project02/app.py
@@ -8,7 +8,8 @@ owlApiKey = tree.find('string[@name="owlbot-key"]').text
 
 app = Flask(__name__)
 
-client = MongoClient('내AWS아이피', 27017, username="test", password="test")
+#client = MongoClient('내AWS아이피', 27017, username="test", password="test")
+client = MongoClient('localhost',27017)
 db = client.dbsparta_plus_week2
 
 
@@ -20,23 +21,38 @@ def main():
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
+
+    status = request.args.get('status')
+
     r = requests.get(f'https://owlbot.info/api/v4/dictionary/{keyword}',
                      headers={'Authorization' : f'Token {owlApiKey}'})
     result = r.json()
 
-    return render_template("detail.html", word=keyword, result=result)
+    return render_template("detail.html", word=keyword, result=result, status=status)
 
 
 @app.route('/api/save_word', methods=['POST'])
 def save_word():
     # 단어 저장하기
-    return jsonify({'result': 'success', 'msg': '단어 저장'})
+
+    word = request.form['word']
+    definition = request.form['definition']
+    doc = {
+        'word':word,
+        'definition':definition
+    }
+
+    db.words.insert_one(doc)
+
+    return jsonify({'result': 'success', 'msg': f'단어 저장 - {word}'})
 
 
 @app.route('/api/delete_word', methods=['POST'])
 def delete_word():
-    # 단어 삭제하기
-    return jsonify({'result': 'success', 'msg': '단어 삭제'})
+    word = request.form['word']
+    db.words.delete_one({'word':word})
+
+    return jsonify({'result': 'success', 'msg': f'단어 삭제 - {word}'})
 
 
 if __name__ == '__main__':

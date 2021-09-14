b10e23eddd6af15c907ba27fd5aa8b4c29e98b09
kimdaehyun <zbeld123@gmail.com>
2021-09-14 16:55:52 +0900

    design detail page

--- project02/app.py
+++ project02/app.py
@@ -1,24 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
 import requests
-import xml.etree.ElementTree as et
-tree = et.parse('keys.xml')
-owlApiKey = tree.find('string[@name="owlbot-key"]').text
+
 
 app = Flask(__name__)
+
+client = MongoClient('내AWS아이피', 27017, username="test", password="test")
+db = client.dbsparta_plus_week2
 
 
 @app.route('/')
 def main():
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
     return render_template("index.html")
 
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
-    r = requests.get(f'https://owlbot.info/api/v4/dictionary/{keyword}', headers={"Authorization": f'Token {owlApiKey}'})
-    result = r.json()
-    print(result)
+    # API에서 단어 뜻 찾아서 결과 보내기
+    return render_template("detail.html", word=keyword)
 
-    return render_template("detail.html")
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    # 단어 저장하기
+    return jsonify({'result': 'success', 'msg': '단어 저장'})
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    # 단어 삭제하기
+    return jsonify({'result': 'success', 'msg': '단어 삭제'})
 
 
 if __name__ == '__main__':
